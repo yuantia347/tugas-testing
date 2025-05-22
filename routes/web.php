46064741d9ec62c62e57e14fdec6e232f543ae92
yuantia347 <yuandhitadita@gmail.com>
2025-05-22 12:10:55 +0800

    Commit

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\CalculatorController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\LoanCalculatorController;
-use App\Http\Controllers\NumberController;
 use App\Http\Controllers\NilaiController;
 use App\Models\Task;
 use Illuminate\Support\Facades\Route;
@@ -25,10 +24,6 @@ Route::post('/calculator', [CalculatorController::class, 'calculate']);
 // Loan
 Route::get('/loan', [LoanCalculatorController::class, 'index']);
 Route::post('/loan', [LoanCalculatorController::class, 'calculate']);
-
-
-Route::get('/number', [NumberController::class, 'index']);
-Route::post('/number/check', [NumberController::class, 'check']);
 
 
 Route::get('/nilai', [NilaiController::class, 'index']);
